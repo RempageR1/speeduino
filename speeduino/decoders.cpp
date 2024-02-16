349b296d84824335de81fbc6036bf9aaa25a3bfc
RempageR1 <remco@remcohobo.nl>
2024-02-16 14:10:54 +0100

    Removed tempRPM

--- speeduino/decoders.cpp
+++ speeduino/decoders.cpp
@@ -4857,12 +4857,6 @@ uint16_t getRPM_Vmax(void)
       tempRPM = stdGetRPM(CRANK_SPEED);
     }
   }
-  if (tempRPM < 1000){
-    BIT_SET(currentStatus.status3, BIT_STATUS3_HALFSYNC);
-  }
-  else{
-    BIT_CLEAR(currentStatus.status3, BIT_STATUS3_HALFSYNC);
-  }
   return tempRPM;
 }
 
